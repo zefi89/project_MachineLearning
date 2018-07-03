53b7158aab7cb6d4ace679208cfa3a3199c099db
Floriana Zefi <zefi@llr.in2p3.fr>
2018-07-03 17:07:32 +0200

    created the folder with variable  histograms

--- sanity_plots.py
+++ sanity_plots.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import os
 import matplotlib.pyplot as plt
 from read_data import read_file
 plt.style.use('fivethirtyeight')
@@ -9,10 +10,9 @@ Making some plots to check for anomalies & understand the data
 
 #check variable distributions
 df = read_file()
+my_path = 'plots'
 
-#store into a file the descriptions for each column 
-
-file=open("output.txt", 'w')  
+file=open("output.txt", 'w')  #store into a file the descriptions for each column
 for column in df:
     out = df[column].describe()
     s = str(out)
@@ -20,11 +20,19 @@ for column in df:
 file.close()
 
 
-#df['PAY_0'].plot()
 
-fig = plt.figure()
-plt.hist(df['AGE'], alpha=0.3)
-plt.title('Age of Client'); plt.xlabel('Age (years)'); plt.ylabel('Count');
-fig.savefig('plots/age_hist.png')
+for column in df:
+    name = list(df[column])
+    plt.hist(df[column], alpha = 0.3)
+    plt.savefig(os.path.join(my_path, "hist_{}.png".format(column)))
+
+"""
+For checking single / specific plot
+"""
+#fig = plt.figure()
+#plt.hist(df['AGE'], alpha=0.3)
+#plt.hist(df['default.payment.next.month'], alpha=0.3)
+#plt.title('Age of Client'); plt.xlabel('Age (years)'); plt.ylabel('Count');
+#fig.savefig('plots/payment_hist.png')
 #plt.show()
 

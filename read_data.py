58ef596a68299340e084d55f90ec3058dde77a21
Floriana Zefi <zefi@llr.in2p3.fr>
2018-07-03 09:24:52 +0200

    added statistics plus some cosmetics about NaN values

--- read_data.py
+++ read_data.py
@@ -9,9 +9,10 @@ from pprint import pprint
 Helpers for reading the data file
 """
 
-fname = 'data/UCI_Credit_Card.csv'
+fname        = 'data/UCI_Credit_Card.csv'
 index_column = 'ID'
-n = 10
+n            = 10
+
 
 def read_file(nan_strategy = None):
 
@@ -32,19 +33,25 @@ def read_file(nan_strategy = None):
     
     (df == 0).astype(int).sum(axis=1)                       #counts the zeros for each row
     
+    
     miss_val = df.isnull().sum()                            #total missing values
     miss_val_fraction = 100 * df.isnull().sum() / len(df)   #percentage of missing values
     miss_val_table = pd.concat([miss_val, miss_val_fraction], axis=1) #create a new table for storing the info about missing values
+    
     miss_val_table = miss_val_table.rename(
         columns = {0 : 'Missing Values', 1 : '% of Total Values'}) #rename missing values
+    
     miss_val_sorted = miss_val_table[miss_val_table.iloc[:,1] != 0].sort_values(       
             '% of Total Values', ascending=False).round(1)  
     
     print("Your selected dataframe has " + str(df.shape[1]) + " columns.\n" 
             "There are " + str(miss_val_sorted.shape[0]) +
             " columns that have missing values.")
+    
     #(df == 'NaN').astype(int).sum(axis=1)
-
+    #df.select_dtypes('object').apply(pd.Series.nunique, axis = 0) #unique classes in each column
+    
+    
     if nan_strategy == 'drop_nan':
         return df.dropna()
     

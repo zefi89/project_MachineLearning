e82559b12c64af95af634ea1b0859c567cd87dbe
Floriana Zefi <zefi@llr.in2p3.fr>
2018-07-03 09:11:30 +0200

    added statistics about NaN values

--- read_data.py
+++ read_data.py
@@ -23,29 +23,35 @@ def read_file(nan_strategy = None):
          raise FileNotFoundError
 
     print(df.head(n))          #print n rows from the data
-    print('Your are working with a file with shape: {}'.format(df.shape), '\n')    
-    
-    #list_col = df.columns.values #creates a list with the column names  
-    print('The column names ===> ' ,df.columns)
+    print('Your are working with a file with shape: {}'.format(df.shape), 'with data type: {}', '\n',df.dtypes.value_counts())   
+
+    print('The column names ===> ', '\n'
+            ,df.columns)
     
     #print'The NaN values: ===>',df.isnull().sum(), '\n'# returns True if any of the values in the column is missing, otherwise it is a false 
     
-    (df == 0).astype(int).sum(axis=1) # counts the zeros for each row
-    (df == 'NaN').astype(int).sum(axis=1)
+    (df == 0).astype(int).sum(axis=1)                       #counts the zeros for each row
+    
+    miss_val = df.isnull().sum()                            #total missing values
+    miss_val_fraction = 100 * df.isnull().sum() / len(df)   #percentage of missing values
+    miss_val_table = pd.concat([miss_val, miss_val_fraction], axis=1) #create a new table for storing the info about missing values
+    miss_val_table = miss_val_table.rename(
+        columns = {0 : 'Missing Values', 1 : '% of Total Values'}) #rename missing values
+    miss_val_sorted = miss_val_table[miss_val_table.iloc[:,1] != 0].sort_values(       
+            '% of Total Values', ascending=False).round(1)  
+    
+    print("Your selected dataframe has " + str(df.shape[1]) + " columns.\n" 
+            "There are " + str(miss_val_sorted.shape[0]) +
+            " columns that have missing values.")
+    #(df == 'NaN').astype(int).sum(axis=1)
 
-    #df_noNan = df.dropna() 
     if nan_strategy == 'drop_nan':
         return df.dropna()
-    
-    df_replacedNan = df.fillna(df.mean())
-    #df_replacedNan = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0) # or median, most_frequent, depending on the case
     
     if nan_strategy == 'fill_na':
         return df.fillna(df.mean())
     
     if nan_strategy == None:
         return df
-
-    #return df
 
 #read_file()
